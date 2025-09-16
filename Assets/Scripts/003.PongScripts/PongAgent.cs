e704ca82b9922a7979d1aff5616266b587b84fa5
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2025-09-16 18:10:37 +0100

    Train Pong Bot and save multiple model versions

--- Assets/Scripts/003.PongScripts/PongAgent.cs
+++ Assets/Scripts/003.PongScripts/PongAgent.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
+
 public class PongAgent : Agent
 {
     [SerializeField] private Rigidbody agentRigidbody;
     [SerializeField] private Transform targetTransform;
     [SerializeField] private Rigidbody targetRigidbody;
     [SerializeField] private float moveSpeed = 5f;
-
     [SerializeField] private float launchForce = 15f;
 
     public override void OnEpisodeBegin()
@@ -17,29 +17,37 @@ public class PongAgent : Agent
         targetTransform.localPosition = new Vector3(Random.Range(-7f, 7f), 0f, 17.25f);
         transform.localPosition = new Vector3(0f, 0f, 0f);
     }
+
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(transform.localPosition);
         sensor.AddObservation(targetTransform.localPosition);
         sensor.AddObservation(targetRigidbody.linearVelocity);
-
     }
+
     public override void OnActionReceived(ActionBuffers actions)
     {
-        // Debug.Log(actions.DiscreteActions[0]); //For Discrete Actions
-        // Debug.Log(actions.ContinuousActions[0]); //For Continuous Actions
+        // Get the discrete action (0: left, 1: idle, 2: right)
+        int discreteAction = actions.DiscreteActions[0];
+        // Convert discrete action to movement value: -1, 0, or 1.
+        float moveX = discreteAction - 1; 
 
-
-        float moveX = actions.ContinuousActions[0];
-        // agentRigidbody.AddForce(new Vector3(moveX, 0, 0) * Time.deltaTime * moveSpeed);
-        transform.localPosition = new Vector3(moveX * 12.5f, 0f, 0f);
-        Debug.Log(moveX);
+        agentRigidbody.AddForce(new Vector3(moveX, 0, 0) * Time.deltaTime * moveSpeed);
+        // Debug.Log("Discrete Action: " + discreteAction);
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
-        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
+        // Use input axis to determine discrete action.
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalInput < 0)
+            discreteActions[0] = 0; // move left
+        else if (horizontalInput > 0)
+            discreteActions[0] = 2; // move right
+        else
+            discreteActions[0] = 1; // no movement
     }
 
     private void OnTriggerEnter(Collider other)
@@ -49,15 +57,11 @@ public class PongAgent : Agent
             Debug.Log("Returned Ball");
             AddReward(2.0f);
         }
-
     }
-
 
     public void Punish()
     {
-        AddReward(-1.0f);
         EndEpisode();
-
         Debug.Log("Punish Called");
     }
 }
