e88ebce1fa692c8e6437399ee1f28da3e683189d
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2025-10-23 16:41:44 +0100

    Add tire mesh that follows suspension raycasts

---
+++ Assets/Scripts/005.DrivingScripts/WheelMesh.cs
@@ -0,0 +1,29 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WheelMesh : MonoBehaviour
+{
+    [SerializeField] GameObject wheelObject;
+    public float restSuspensionDistance = 0.3f;
+
+    public float offsetY = 0.05f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit, restSuspensionDistance))
+        {
+            // Convert hit.point from world space to local space relative to the current transform
+            Vector3 localHitPoint = transform.InverseTransformPoint(hit.point);
+            Vector3 tireRawPos = localHitPoint + (Vector3.up + new Vector3(0, offsetY, 0));
+            wheelObject.transform.localPosition = Vector3.Lerp(wheelObject.transform.localPosition, tireRawPos, 0.5f);
+        }
+    }
+}
